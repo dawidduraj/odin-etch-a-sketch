1b9b8c684e381123e7d37df607f16a3141ada795
Dawid Duraj <ItsHailight@gmail.com>
2022-04-10 11:40:23 +0200

    Create dynamic grid template resizing

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // DOM elements
-const container = document.querySelector(".container");
+const container = document.querySelector("#container");
 const sizeDisplay = document.querySelector(".size");
 const slider = document.querySelector("#gridSizeSlider");
 
 let gridSize = 16;
 
 slider.oninput = () => {
-    console.log(slider.value);
     gridSize = slider.value;
     sizeDisplay.textContent = `Grid Size: ${gridSize}`
+    createGrid(gridSize)
+}
+
+function createGrid(size){
+    container.style.setProperty("--grid-size", size);
 }
