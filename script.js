7933496917444ec3882f2ba9f5c7335b9d326f48
Dawid Duraj <ItsHailight@gmail.com>
2022-04-10 11:59:18 +0200

    Implement grid clearing

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const container = document.querySelector("#container");
 const sizeDisplay = document.querySelector(".size");
 const slider = document.querySelector("#gridSizeSlider");
+const button = document.querySelector("button");
 
 let gridSize = 16;
 
@@ -11,13 +12,25 @@ createGrid(gridSize);
 slider.oninput = () => {
     gridSize = slider.value;
     sizeDisplay.textContent = `Grid Size: ${gridSize}`
+    clearGrid();
     createGrid(gridSize)
 }
+
+button.onclick = clearGrid;
 
 function createGrid(size){
     container.style.setProperty("--grid-size", size);
     for (let i = 0; i < (size * size); i++){
         let cell = document.createElement("div");
+        cell.onmouseenter = () => {
+            cell.classList.add("colored");
+        }
         container.appendChild(cell);
     }
+}
+
+function clearGrid(){
+    container.childNodes.forEach((child) => {
+        child.classList.remove("colored");
+    })
 }
